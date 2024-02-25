9663a1a43ff1a5a1ec323ddb154b49af109acafd
mushfiqur1500 <mushfiqur1500@gmail.com>
2024-02-25 10:07:19 +0600

    nest work

--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import ReactDom from 'react-dom/client';
 
 function Greeting() {
   return (
-    <>
-    <div classNname='someValue'>
-      <h3>My First Project</h3>
-      <h3>Hello people</h3>
-      <ul>
-        <li>
-          <a href='#'>Hello World</a>
-        </li>
-      </ul>
+    <div>
+      <Person />
+      <Message />
     </div>
-    <h2>Good Morning</h2>
-    <input type='text' name='' id=''/>
-  </>
-  );
+    );
 }
 
+const Person = () => <h2>My Name Is Ullas</h2>;
+const Message = () => {
+  return <p>this is my idea</p>;
+};
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<Greeting />);
