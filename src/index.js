13b53574f545e7cbc8a5c9f478648b63ec8017ce
mushfiqur1500 <mushfiqur1500@gmail.com>
2024-02-27 23:16:14 +0600

    index.css update

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const BookList = () => {
+
+import './index.css';
+
+
+function BookList() {
   return (
-    <>
+    <section className='booklist'>
       <Book />
-    </>
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
-};
+}
+
 const Book = () => {
   return (
-    <>
+    <article className='book'>
       <Image />
       <Title />
       <Author />
-    </>
+    </article>
   );
 };
-const Image = () => {
-  return (
-    <>
-      <h2>Image PlaceHolder</h2>
-    </>
-  );
-};
-const Title = () => {
-  return (
-    <>
-      <h2>Book Title</h2>
-    </>
-  );
-};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81JL3vN6voL._AC_UL600_SR600,400_.jpg"
+    alt="Blood Money"
+  />
+);
+const Title = () => <h2>Blood Money</h2>;
 const Author = () => {
-  return (
-    <>
-      <h2>Author</h2>
-    </>
-  );
+  return <h4>Peter Schweizer</h4>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<BookList />);
