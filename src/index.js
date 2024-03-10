057f94bd868b65f913c3032eb60618f5beb0b542
Mohammad Mushfiqur Rahman <mushfiqur1500@gmail.com>
2024-03-10 19:53:16 +0600

    css adding

--- src/index.js
+++ src/index.js
@@ -33,11 +33,12 @@ const Image = () => (
 );
 const Title = () => <h2>Blood Money</h2>;
 const Author = () => {
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
-      Peter Schweizer
-    </h4>
-  );
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+  return <h4 style={inlineHeadingStyles}>Peter Schweizer </h4>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
